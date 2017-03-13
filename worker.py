73b66e2b26a5c2821a69e35edf252b6497548d1a
Alexander Gor <ornot.work@gmail.com>
2017-03-13 20:35:36 +0200

    system task that runs custom functions dynamicaly

--- worker.py
+++ worker.py
@@ -17,13 +17,31 @@ tasks = glob.glob(tasks_path)
 # @todo: unwrap the magic
 app.conf.imports = ['tasks.' + os.path.basename(f)[:-3] for f in tasks if os.path.isfile(f) and f != '__init__']
 
+
+@app.task
+def dynamicTask(taskname, taskargs):
+    # We cant load function directly, so splitting name
+    modulename, funcname = taskname.rsplit('.', 1)
+
+    module = importlib.import_module(modulename)
+    func = getattr(module, funcname)
+
+    # arguments can be proceed as positional list or key-value
+    if isinstance(taskargs, dict):
+        return func(**taskargs)
+    elif isinstance(taskargs, list):
+        return func(*taskargs)
+
+
 @app.on_after_configure.connect
 def after_configure(**kwargs):
     pass
 
+
 @signals.task_success.connect
 def task_success(**kwargs):
     pass
+
 
 @signals.task_failure.connect
 def task_success(**kwargs):
