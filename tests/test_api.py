4ffbf43d6a8f4203100d674ba0a9576c8a27742c
Alexander Gor <ornot.work@gmail.com>
2017-03-14 14:46:11 +0200

    tests, we must not add task with empty data

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,24 @@
+import os
+import json
+import unittest
 
-import nose
+from flask.testing import FlaskClient
+import app as celery_tm
 
+
+class TestClient(FlaskClient):
+    def open(self, *args, **kwargs):
+        if 'json' in kwargs:
+            kwargs['data'] = json.dumps(kwargs.pop('json'))
+            kwargs['content_type'] = 'application/json'
+
+        return super(TestClient, self).open(*args, **kwargs)
+
+
+class APITestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = celery_tm.app.test_client()
+
+    def test_cant_add_empty_task(self):
+        res = self.app.post('/api/v1.0/schedule')
+        assert res.status_code == 400
