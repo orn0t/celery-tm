fbb1b854150c333c5154893564b38e2139cf7a2a
Alexander Gor <ornot.work@gmail.com>
2017-03-15 16:33:57 +0200

    adding recurring flag based on schedule type

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import json
 import time
 from sqlalchemy import event
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.hybrid import hybrid_property
 
 db = SQLAlchemy()
 
@@ -25,21 +26,32 @@ class TaskModel(db.Model, BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     created = db.Column(db.DateTime, default=db.func.now())
     function = db.Column(db.String(64))
-    run_type = db.Column(db.String(32))
-    schedule = db.Column(db.String(64))
     args = db.Column(db.Text)
     last_run = db.Column(db.String(32))
     exit_code = db.Column(db.String(64))
-    # @todo: find out how to save uuid in more proper way
     uuid = db.Column(db.String(64))
+    recurring = db.Column('recurring', db.Integer, default=0)
+
+    _schedule = db.Column('schedule', db.String(64))
 
     def __init__(self, data):
         self.function = data['function']
-        self.run_type = ''
         self.schedule = data['schedule']
 
         if 'args' in data.keys():
             self.args = json.dumps(data['args'])
+
+    @hybrid_property
+    def schedule(self):
+        return self._schedule
+
+    @schedule.setter
+    def schedule(self, schedule):
+        self._schedule = schedule
+
+        if schedule != 'now':
+            self.recurring = 1
+
 
 class LogItemModel(db.Model, BaseModel):
     __tablename__ = 'logs'
